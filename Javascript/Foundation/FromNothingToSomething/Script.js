5846ed6c1d9c684b7d08c7950694aaa024dd0649
Shubham Gaikwad <shubhamgaikwad3297@gmail.com>
2025-08-11 20:40:47 +0530

    Js Animation - Event Handling

--- Javascript/Foundation/FromNothingToSomething/Script.js
+++ Javascript/Foundation/FromNothingToSomething/Script.js
@@ -31,9 +31,34 @@ console.log("a", a);
 
 // document.querySelector("body").appendChild(h1);
 
-var img = document.createElement("img");
-img.src =
-  "https://plus.unsplash.com/premium_photo-1754398386796-ea3dec2a6302?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+// var img = document.createElement("img");
+// img.src =
+// "https://plus.unsplash.com/premium_photo-1754398386796-ea3dec2a6302?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-document.querySelector("body").appendChild(img);
-document.querySelector("body").removeChild(img);
+// document.querySelector("body").appendChild(img);
+// document.querySelector("body").removeChild(img);
+
+// Event Handling
+
+var btn = document.querySelector("button");
+// btn.addEventListener(event, callback);
+// btn.addEventListener("click", function () {
+//   // alert();
+//   btn.textContent = "Uploading";
+//   btn.style.backgroundColor = "red";
+// });
+
+// btn.addEventListener("dblclick", function () {
+//   btn.textContent = "Uploading";
+//   btn.style.backgroundColor = "red";
+// });
+
+btn.addEventListener("mouseover", function () {
+  btn.textContent = "Uploading";
+  btn.style.backgroundColor = "red";
+});
+
+btn.addEventListener("mouseleave", function () {
+  btn.textContent = "Uploading";
+  btn.style.backgroundColor = "green";
+});
