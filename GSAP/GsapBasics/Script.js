1c6f1220cdb72ebc7dbd59c1a0381b87e62cb8a6
Shubham Gaikwad <shubhamgaikwad3297@gmail.com>
2025-08-20 20:20:54 +0530

    GSAp- initial to final, final to initial

---
+++ GSAP/GsapBasics/Script.js
@@ -0,0 +1,19 @@
+// console.log("first");
+
+// 1st Method - Initial to Final
+gsap.to("#box", {
+  x: 1000,
+  rotate: 360,
+  backgroundColor: "blue",
+  duration: 4,
+  delay: 1,
+});
+
+// 2nd Method - Final to Initial
+// gsap.from("#box", {
+//   x: 1000,
+//   rotate: 360,
+//   backgroundColor: "blue",
+//   duration: 4,
+//   delay: 1,
+// });
