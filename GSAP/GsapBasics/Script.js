de86434044460f7f5db8d0ded33417ef01b9c9b0
Shubham Gaikwad <shubhamgaikwad3297@gmail.com>
2025-08-20 20:54:58 +0530

    GSAP - Timeline

--- GSAP/GsapBasics/Script.js
+++ GSAP/GsapBasics/Script.js
@@ -1,13 +1,13 @@
 // console.log("first");
 
 // 1st Method - Initial to Final
-gsap.to("#box", {
-  x: 1000,
-  rotate: 360,
-  backgroundColor: "blue",
-  duration: 4,
-  delay: 1,
-});
+// gsap.to("#box", {
+//   x: 1000,
+//   rotate: 360,
+//   backgroundColor: "blue",
+//   duration: 4,
+//   delay: 1,
+// });
 
 // 2nd Method - Final to Initial
 // gsap.from("#box", {
@@ -17,3 +17,22 @@ gsap.to("#box", {
 //   duration: 4,
 //   delay: 1,
 // });
+
+// Timeline
+var t1 = gsap.timeline();
+
+t1.to("#box1", {
+  x: 1000,
+  rotate: 360,
+  scale: 0.5,
+  duration: 2,
+  delay: 1,
+});
+
+t1.to("#box2", {
+  x: 1000,
+  rotate: 360,
+  scale: 0.5,
+  duration: 1,
+});
+// Timeline
